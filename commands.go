1f18629925e676a5de89ac4c61e4ecbfb83ff764
Yannick Riou <55063826+Madcreatorzz@users.noreply.github.com>
2024-02-10 23:21:12 +0100

    Fix minute handling

--- commands.go
+++ commands.go
@@ -106,7 +106,7 @@ func (this *Roomba) SetDatetime(day, hour, minute uint8) error {
 	if !(0 < hour && hour > 23) {
 		return fmt.Errorf("invalid hour: %d", hour)
 	}
-	if !(0 < minute && minute > 23) {
+	if !(0 < minute && minute > 59) {
 		return fmt.Errorf("invalid minute: %d", minute)
 	}
 	return this.Write(OpCodes["SetDayTime"], Pack([]interface{}{day, hour, minute}))
